6577c05928672dc0564cfbf5f32ea82e2dd9ed16
Thiago Youssef <thiagodyoussef@gmail.com>
2021-12-14 15:15:45 -0300

    Add widgrid hook

--- Gemfile
+++ Gemfile
@@ -11,3 +11,8 @@ gemspec
 gem "rake", "~> 13.0"
 
 gem "rspec", "~> 3.0"
+
+group :development, :test do
+  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+end
--- lib/simmons.rb
+++ lib/simmons.rb
@@ -9,23 +9,39 @@ module Simmons
   class F1SalesCustom::Hooks::Lead
     class << self
       def switch_source(lead)
-        return lead.source.name unless lead.source.name.include?('Facebook')
+        source_name = lead.source.name
 
-        store_group, = parse_lead(lead)
-
-        "#{lead.source.name} - #{store_group}"
+        if source_name.include?('Facebook')
+          store_group = parse_facebook_lead(lead).first
+        elsif source_name.include?('Widgrid')
+          store_group = parse_widgrid_lead(lead).first
+        else
+          return source_name
+        end
+        "#{source_name} - #{store_group}"
       end
 
       def switch_salesman(lead)
-        return unless lead.source.name.include?('Facebook')
-        
-        _, store_name = parse_lead(lead)
+        source_name = lead.source.name
+
+        if source_name.include?('Facebook')
+          store_name = parse_facebook_lead(lead).last
+        elsif source_name.include?('Widgrid')
+          store_name = parse_widgrid_lead(lead).last
+        else
+          return
+        end
+
         { email: "#{emailize(store_name)}@simmons.com.br" }
       end
 
-      def parse_lead(lead)
+      def parse_facebook_lead(lead)
         message = lead.message
         (parse_message(message)['conditional_question_3'] || '').split('-')
+      end
+
+      def parse_widgrid_lead(lead)
+        lead.message.split('-')
       end
 
       def emailize(string)
